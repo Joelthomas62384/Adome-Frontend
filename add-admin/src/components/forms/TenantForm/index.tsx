13b51c9bbade538383b07c600aa192a6d0a9e370
Joel Thomas <joel@codewithjoe.in>
2025-02-15 04:39:29 +0530

    started tenantform

---
+++ add-admin/src/components/forms/TenantForm/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form"
+  import { TenantFormType } from '@/types'
+  import { Input } from "@/components/ui/input"
+  import { SubmitHandler, UseFormReturn } from 'react-hook-form'
+
+type Props = {}
+
+const TenantForm = (props: Props) => {
+  return (
+    <div>TenantForm</div>
+  )
+}
+
+export default TenantForm
--- add-admin/src/types/index.ts
+++ add-admin/src/types/index.ts
@@ -1,3 +1,11 @@
-export interface TenantForm {
-    
+export interface TenantFormType {
+    name : string
+    logo : string
+    domain : string
+    founding_year : string
+    location : string
+    description : string
+    blog: boolean
+    community : boolean
+    newsletter : boolean
 }
